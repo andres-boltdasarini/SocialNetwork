b42ee363d2875bdd5078954d3bb1f230232d7770
Андрей <lau47@mail.ru>
2025-06-19 12:20:55 +0300

    refactoring1

---
+++ SocialNetwork/BLL/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace SocialNetwork.BLL.Models
+{
+    public class Message
+    {
+        public int Id { get; }
+        public string Content { get; set; }
+        public int Sender_Id { get; }
+        public int Recipient_Id { get; set; }
+
+
+        public Message(
+            int id,
+            string content,
+            int sender_id,
+            int recipient_id)
+        {
+            this.Id = id;
+            this.Content = content;
+            this.Sender_Id = sender_id;
+            this.Recipient_Id = recipient_id;
+        }
+    }
+}
---
+++ SocialNetwork/PLL/Views/AuthenticationView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AuthenticationView
+    {
+        UserService userService;
+        public AuthenticationView(UserService userService)
+        {
+            this.userService = userService;
+        }
+        public void Show()
+        {
+            var authenticationData = new UserAuthenticationData();
+
+            Console.WriteLine("Введите почтовый адрес:");
+            authenticationData.Email = Console.ReadLine();
+
+            Console.WriteLine("Введите пароль:");
+            authenticationData.Password = Console.ReadLine();
+
+            try
+            {
+                User user = this.userService.Authenticate(authenticationData);
+                SuccessMessage.Show("Вы успешно вошли");
+                SuccessMessage.Show("Добро пожаловать " + user.FirstName);
+
+                Program.userMenuView.Show();
+            }
+            catch (WrongPasswordException ex)
+            {
+                AlertMessage.Show($"Пароль не корректный: {ex.Message}");
+            }
+            catch (UserNotFoundException ex)
+            {
+                AlertMessage.Show($"Пользователь не найден: {ex.Message}");
+            }
+        }
+    }
+}

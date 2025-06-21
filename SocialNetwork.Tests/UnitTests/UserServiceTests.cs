1a46304bfd5292a0a6839b1435276986562e627d
Андрей <lau47@mail.ru>
2025-06-21 12:58:37 +0300

    tests_init

---
+++ SocialNetwork.Tests/UnitTests/UserServiceTests.cs
@@ -0,0 +1,15 @@
+using Moq;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+
+[TestFixture]
+public class UserServiceTests
+{
+    [Test]
+    public void Register_ExistingEmail_ThrowsException()
+    {
+
+    }
+}
